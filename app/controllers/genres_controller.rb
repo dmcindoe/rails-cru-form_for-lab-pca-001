735d731d834aa0141eee52ff9e98b2baa29cba22
Andrew McIndoe <amcindoe@powerhrg.com>
2021-09-22 13:49:40 -0400

    complete

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,32 @@
+class GenresController < ApplicationController
+  before_action :set_genre, only: [:show, :edit, :update]
+
+    def show
+    end
+  
+    def new
+    end
+  
+    def create
+      @genre = Genre.create(genre_params)
+      redirect_to genre_path(@genre)
+    end
+  
+    def edit
+    end
+  
+    def update
+      @genre.update(genre_params)
+      redirect_to genre_path(@genre)
+    end
+  
+    private
+  
+    def genre_params
+      params.require(:genre).permit(:name, :bio)
+    end
+  
+    def set_genre
+      @genre = Genre.find(params[:id])
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+rise = Artist.create(name: "Rise Against", bio: "Leaders of the Nowhere Generation")
+nonpoint = Artist.create(name: "Nonpoint", bio: "Industial done right")
+rustage = Artist.create(name: "Rustage", bio: "1st Hokage of Nerdcore")
+hamm = Artist.create(name: "Hamm Sandwich", bio: "Off the Wall, and well loves going Ham")
+
+nerdcore = Genre.create(name: "Nerdcore")
+rock = Genre.create(name: "Rock")
+industrial = Genre.create(name: "Industrial Rock")
+
+Song.create(name: "Help is On the Way", artist: rise, genre: rock)
+Song.create(name: "Love is War", artist: hamm, genre: nerdcore)
+Song.create(name: "Awake", artist: rustage, genre: nerdcore)
+Song.create(name: "What A Day", artist: nonpoint, genre: industrial)
+Song.create(name: "Bullet with a Name", artist: nonpoint, genre: industrial)
